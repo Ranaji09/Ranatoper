dd6967995581153b2126fabdad7c60553f5c0c6c
Rana ji <ranabrajesh76@gmail.com>
2026-05-16 23:32:29 +0530

    Create ads.js

---
+++ js/ads.js
@@ -0,0 +1,46 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+  // ===============================
+  // AD UNIT 1
+  // ===============================
+  (function(s){
+
+    s.dataset.zone = '11015831';
+    s.src = 'https://nap5k.com/tag.min.js';
+
+  })(
+    [document.documentElement, document.body]
+    .filter(Boolean)
+    .pop()
+    .appendChild(document.createElement('script'))
+  );
+
+
+  // ===============================
+  // VIGNETTE AD
+  // ===============================
+  (function(s){
+
+    s.dataset.zone = '11015822';
+    s.src = 'https://n6wxm.com/vignette.min.js';
+
+  })(
+    [document.documentElement, document.body]
+    .filter(Boolean)
+    .pop()
+    .appendChild(document.createElement('script'))
+  );
+
+
+  // ===============================
+  // AD UNIT 3
+  // ===============================
+  const ad3 = document.createElement("script");
+
+  ad3.src = "https://5gvci.com/act/files/tag.min.js?z=11015834";
+  ad3.async = true;
+  ad3.setAttribute("data-cfasync", "false");
+
+  document.body.appendChild(ad3);
+
+});
